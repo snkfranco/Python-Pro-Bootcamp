2a62d8fb9f83e0ff8691e4a09d78afbc8f0a7721
Matheus Franco <matheusmfranco.mf@gmail.com>
2024-03-14 09:18:38 -0300

    Add files via upload

---
+++ Day 1 - Simple Things/main.py
@@ -0,0 +1,9 @@
+import os
+
+os.system('cls')
+
+print("Welcome to the band name generator!\n")
+city = input("What's the name of your city? >> ")
+pet = input("What's the name of your pet? >> ")
+
+print(f"\nThe name of your band could be: {city} {pet}")
