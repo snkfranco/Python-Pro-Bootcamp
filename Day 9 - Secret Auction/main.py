5362230520c1f8ce05b89d8bd27425cccf553f11
Matheus Franco <matheusmfranco.mf@gmail.com>
2024-04-21 02:58:22 -0300

    Add files via upload

---
+++ Day 9 - Secret Auction/main.py
@@ -0,0 +1,26 @@
+import os
+
+bids = {}
+bidding_finished = False
+
+def find_highest_bidder (bidding_record):
+    highest_bid = 0
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} and the bid was ${highest_bid}")
+
+while not bidding_finished:
+    name = input("What is your name? ")
+    price = int(input("What is your bid? $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no': ")
+    if should_continue == 'no':
+        bidding_finished = True
+        find_highest_bidder(bids)
+        
+
+    elif should_continue == 'yes':
+        os.system('cls')
